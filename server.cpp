8edd987bb885afc2737ea5806bec7619adf4ee96
Deodand <deodand@mail.ru>
2018-01-29 14:15:00 -0800

    d

--- server.cpp
+++ server.cpp
@@ -17,7 +17,7 @@ int main(int argc, char* argv[]) {
     if(argc > 1) 
         portNum = atoi(argv[1]);
     else
-        portNum = 150010;
+        portNum = 15000;
 
     struct sockaddr_in server_addr;
     socklen_t sizeServerAddress;
